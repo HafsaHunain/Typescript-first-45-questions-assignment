c123c6f4767f713a2abf2148fefd66936b226a48
HafsaHunain <166612833+HafsaHunain@users.noreply.github.com>
2024-04-11 13:40:29 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+let quoteWords = "A person who never made a mistake never tried  anything new";
+let authorName = "Albert Einstein";
+console.log(`${authorName} once said,"${quote}`);
